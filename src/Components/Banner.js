c8fc226e5cf88c841b169aebc8e5ddb2e4bffabf
shahryar-cmyk <58168504+shahryar-cmyk@users.noreply.github.com>
2023-10-28 13:27:28 +0500

    dynamic data of the banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -110,27 +110,71 @@ function Banner() {
     const data = [
         {
             id: '1',
-            image: image2,
+            className: 'active',
+            image: image15,
+            name: 'Shahryar',
+            movieTitle: 'Movie Title 2',
+            year: '2023',
+            ageRating: '15+',
+            duration: '2h 5min',
+            genre: 'Action',
+            description: 'Lorem Ipsum is simply dummy text...',
+            movieTitleimage: image5,
             bgImg: image8,
         },
         {
             id: '2',
-            image: image3,
+            className: '',
+            image: image7,
+            name: 'Akash',
+            movieTitle: 'Movie Title 1',
+            year: '2023',
+            ageRating: '12+',
+            duration: '2h 16min',
+            genre: 'Romance',
+            description: 'Lorem Ipsum is simply dummy text...',
+            movieTitleimage: image2,
             bgImg: image9,
         },
         {
             id: '3',
-            image: image4,
+            className: '',
+            image: image14,
+            name: 'Ahmed',
+            movieTitle: 'Movie Title 3',
+            year: '2023',
+            ageRating: '10+',
+            duration: '1h 50min',
+            genre: 'War/action',
+            movieTitleimage: image4,
             bgImg: image10,
         },
         {
             id: '4',
-            image: image5,
+            className: '',
+            image: image13,
+            name: 'Afaq',
+            movieTitle: 'Movie Title 4',
+            year: '2023',
+            ageRating: '12+',
+            duration: '2h 20min',
+            genre: 'Thriller',
+            description: 'Lorem Ipsum is simply dummy text...',
+            movieTitleimage: image3,
             bgImg: image11,
         },
         {
             id: '5',
-            image: image6,
+            className: '',
+            image: image16,
+            name: 'Abdul rafay',
+            movieTitle: 'Movie Title 5',
+            year: '2023',
+            ageRating: '11+',
+            duration: '2h 00min',
+            genre: 'Horror',
+            description: 'Lorem Ipsum is simply dummy text...',
+            movieTitleimage: image6,
             bgImg: image12,
         },
 
@@ -143,7 +187,7 @@ function Banner() {
 
 
         <div >
-            <div className='header'>
+            {/* <div className='header'>
                 <a href="#" class="logo">Movies</a>
                 <ul class="nav">
                     <li><a href="#"><i class="fa-solid fa-house"></i></a></li>
@@ -159,138 +203,61 @@ function Banner() {
 
                 </div>
 
-            </div>
+            </div> */}
             <div class="banner">
-                {/* {
-                    data.map(({ image, bgimage, id }) => {
-                        return (
-                            <>
-                                {toggled === id ? (
 
-
-
-                                    <img src={image} alt=" " className="img"></img>
-
-
-
-                                ) : null}
-
-                            </>
-                        )
-                    })
-                } */}
-                <div class="content the-little-mermaid active">
-
-                    <img src={image5} class="movie-title" ></img>
-                    {/* <h4>
-                            <span>2023</span><span><i>12+</i></span>
-                            <span>2h 16min</span><span>Romance</span>
-                        </h4> */}
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-                    </p>
-                    <div class="button">
-                        <a href="#"><i class="fa-solid fa-play"></i>Watch</a>
-                        <a href="#"><i class="fa-solid fa-plus"></i>My List</a>
-
-                    </div>
-
-                </div>
-                <div class="content bg-65">
-                    <img src={image2} class="movie-title" alt="Movie Name"></img>
-                    <h4>
-                        <span>2023</span>
-                        <span><i>15+</i></span>
-                        <span>2h 5min</span>
-                        <span>Action</span>
-                    </h4>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-                    </p>
-                    <div class="button">
-                        <a href="#"><i class="fa-solid fa-play"></i>Watch</a>
-                        <a href="#"><i class="fa-solid fa-plus"></i>My List</a>
-
-                    </div>
-
-                </div>
-                <div class="content the-covenant">
-                    <img src={image4} class="movie-title" alt="Movie Name"></img>
-                    <h4>
-                        <span>2023</span>
-                        <span><i>10+</i></span>
-                        <span>1h 50min</span>
-                        <span>War/action</span>
-                    </h4>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-                    </p>
-                    <div class="button">
-                        <a href="#"><i class="fa-solid fa-play"></i>Watch</a>
-                        <a href="#"><i class="fa-solid fa-plus"></i>My List</a>
-
-                    </div>
-
+                <div>
+                    {data.map((item, index) => (
+                        <div key={index} className={`content ${item.className}`}>
+                            <img src={item.movieTitleimage} className="movie-title" alt="Movie Title" />
+                            <h4>
+                                <span>{item.year}</span>
+                                <span><i>{item.ageRating}</i></span>
+                                <span>{item.duration}</span>
+                                <span>{item.genre}</span>
+                            </h4>
+                            <p>{item.description}</p>
+                            <div className="button">
+                                <a href="#"><i className="fa-solid fa-play"></i>Watch</a>
+                                <a href="#"><i className="fa-solid fa-plus"></i>My List</a>
+                            </div>
+                        </div>
+                    ))}
                 </div>
 
-                <div class="content the-black-demo">
-                    <img src={image3} class="movie-title" alt="Movie Name"></img>
-                    <h4>
-                        <span>2023</span>
-                        <span><i>12+</i></span>
-                        <span>2h 20min</span>
-                        <span>Thriller</span>
-                    </h4>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-                    </p>
-                    <div class="button">
-                        <a href="#"><i class="fa-solid fa-play"></i>Watch</a>
-                        <a href="#"><i class="fa-solid fa-plus"></i>My List</a>
-
-                    </div>
-
-                </div>
-                <div class="content the-tank">
-                    <img src={image5} class="movie-title" alt="Movie Name"></img>
-                    <h4>
-                        <span>2023</span>
-                        <span><i>11+</i></span>
-                        <span>2h 00min</span>
-                        <span>Horror</span>
-                    </h4>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-                    </p>
-                    <div class="button">
-                        <a href="#"><i class="fa-solid fa-play"></i>Watch</a>
-                        <a href="#"><i class="fa-solid fa-plus"></i>My List</a>
-
-                    </div>
-
-                </div>
                 <div class="carousel-box">
                     <div class="carousel">
-                    <div class="carousel-item" onClick="changeBg('bg-little-mermaid.jpg', 'the-little-mermaid');">
-                    <img src={image15} alt=""/>
+                        {data.map((item, index) => (
+                            <div
+                                key={index}
+                                className="carousel-item"
+                                onClick={() => {
+                                    console.log(item.name);
+                                    // return changeBg(item.image, item.name);
+                                }}
+                            >
+                                <img src={item.image} alt="" />
+                            </div>
+                        ))}
+                        {/* <div class="carousel-item" onClick="changeBg('bg-little-mermaid.jpg', 'the-little-mermaid');">
+                            <img src={image15} alt="" />
+                        </div>
+                        <div class="carousel-item" onClick="changeBg('bg-65.jpeg', 'bg-65');">
+                            <img src={image7} alt="" />
 
-                </div>
-                <div class="carousel-item" onClick="changeBg('bg-65.jpeg', 'bg-65');">
-                    <img src={image7} alt=""/>
+                        </div>
+                        <div class="carousel-item" onClick="changeBg('bg-the-covenant.jpeg', 'the-covenant');">
+                            <img src={image14} alt="" />
 
-                </div>
-                <div class="carousel-item" onClick="changeBg('bg-the-covenant.jpeg', 'the-covenant');">
-                    <img src={image14} alt=""/>
+                        </div>
+                        <div class="carousel-item" onClick="changeBg('bg-the-black-demon.jpeg', 'the-black-demo');">
+                            <img src={image13} alt="" />
 
-                </div>
-                <div class="carousel-item" onClick="changeBg('bg-the-black-demon.jpeg', 'the-black-demo');">
-                    <img src={image13} alt=""/>
+                        </div>
+                        <div class="carousel-item" onClick="changeBg('bg-the-tank.jpeg', 'the-tank');">
+                            <img src={image16} alt="" />
 
-                </div>
-                <div class="carousel-item" onClick="changeBg('bg-the-tank.jpeg', 'the-tank');">
-                    <img src={image16} alt=""/>
-
-                </div>
+                        </div> */}
                     </div>
                 </div>
                 <a href="#" class="play" onClick={{}
